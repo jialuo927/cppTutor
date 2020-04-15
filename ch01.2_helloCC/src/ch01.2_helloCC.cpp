6576d739327514d9237ce9c7746218cc8ed19fd9
jluo <jialuo927@sina.com>
2020-04-15 21:40:03 +0800

    Chapter 01.2 example

---
+++ ch01.2_helloCC/src/ch01.2_helloCC.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : 2_helloCC.cpp
+// Author      : Jia Luo
+// Version     :
+// Copyright   : SGG of WHU 2020-2025
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello C++" << endl; // prints Hello C++
+	return 0;
+}
